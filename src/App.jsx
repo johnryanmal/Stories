8fab8609b3bce0146f72ddb4856fc9a8c4194880
johnryanmal <johnryanmal@gmail.com>
2023-02-06 11:48:06 -0800

    proto signup / login / logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 import { useState } from 'react'
 import axios from 'axios'
 
+import { Signup } from './Signup.jsx'
+import { Login } from './Login.jsx'
+import { Logout } from './Logout.jsx'
+
 export default function App() {
   const [ story, setStory ] = useState({})
 
@@ -11,6 +15,9 @@ export default function App() {
 
   return (
     <div className="App">
+      <Signup />
+      <Login />
+      <Logout />
       <p>{JSON.stringify(story)}</p>
     </div>
   )
