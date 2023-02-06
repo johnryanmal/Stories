faa60dffbe788a9f293b2bc8ea9336fa398b1ab5
johnryanmal <johnryanmal@gmail.com>
2023-02-06 11:37:01 -0800

    story state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
 
+import { useState } from 'react'
+
 export default function App() {
+  const [ story, setStory ] = useState({})
+
+  const getStory = () => {
+  }
+
   return (
     <div className="App">
+      <p>{JSON.stringify(story)}</p>
     </div>
   )
 }
