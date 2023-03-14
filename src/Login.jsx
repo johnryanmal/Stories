7845ef7436cf356985835a627aebe4d4a1bf1cf4
johnryanmal <johnryanmal@gmail.com>
2023-03-13 17:20:23 -0700

    refactor host into config (2)

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,3 +1,4 @@
+import config from './config'
 import axios from "axios";
 import { useState } from "react";
 
@@ -14,7 +15,7 @@ export function Login(props) {
     setErrors([]);
     const params = new FormData(event.target);
     axios
-      .post("http://localhost:3000/sessions.json", params)
+      .post(`${config.host}/sessions.json`, params)
       .then((response) => {
         //console.log(response.data);
         axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
