8bc3cd3a5178da75ee00aeb95a702fddd1845483
johnryanmal <johnryanmal@gmail.com>
2023-03-13 21:36:12 -0700

    use hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/stories">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
