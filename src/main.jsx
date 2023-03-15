e720eb9823e1dfae664dc08a64a9ab60e7d5dac1
johnryanmal <johnryanmal@gmail.com>
2023-03-15 12:07:15 -0700

    upgrade to react@^18.2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 )
